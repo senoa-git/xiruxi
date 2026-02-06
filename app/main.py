cbc048b05b3d91a8519e62a1bfb07aea4e74c873
suji <konosuba20040114@icloud.com>
2026-02-06 21:10:36 +0900

    style | pixel ocean

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from .database import init_db
 from .routes import router
 
 app = FastAPI(title="Drift Bottle")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.on_event("startup")
